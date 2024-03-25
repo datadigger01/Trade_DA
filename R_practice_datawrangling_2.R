b6d37177cc87b44aee25f3bf70203356384ba470
datadigger01 <143661820+datadigger01@users.noreply.github.com>
2024-03-26 06:46:28 +0900

    Add files via upload

--- R_practice_datawrangling_2.R
+++ R_practice_datawrangling_2.R
@@ -6,7 +6,7 @@ df <- read.csv("./wdi_data.csv")
 df %>% filter(!region %in% c("","Aggregates")) %>%
        select(country:year, EXPORT_LCU) %>%
        arrange(country, year) %>%
-  pivot_wider( names_from = year, values_from = EXPORT_LCU ) %>% view()
+    pivot_wider( names_from = year, values_from = EXPORT_LCU) %>% view()
       
 
 df %>% filter(!region %in% c("","Aggregates")) %>%
@@ -18,8 +18,9 @@ df %>% filter(!region %in% c("","Aggregates")) %>%
 # 2. pivot_longer()
 df %>% filter(region=='East Asia & Pacific') %>%
   select(country,iso3c, region, year, EXPORT_LCU, IMPORT_LCU) %>%
-  arrange(country, year) %>% 
+  arrange(country, year) %>%
   pivot_longer( cols = c(EXPORT_LCU, IMPORT_LCU), names_to = "EXP_IMP", values_to = "AMT_LCU") %>% view()
+
 
 df %>% filter(region=='East Asia & Pacific') %>%
   select(country,iso3c, region, year, TRADE_IN_GDP:IMPORT_USD) %>%
@@ -27,20 +28,29 @@ df %>% filter(region=='East Asia & Pacific') %>%
   pivot_longer( cols = TRADE_IN_GDP:IMPORT_USD, names_to = "TRADE", values_to = "AMT") %>% view()
 
 
+## boxplot graph after pivot_longer
+df %>% filter(region=='East Asia & Pacific') %>%
+  select(country,iso3c, region, year, EXPORT_LCU, IMPORT_LCU) %>%
+  arrange(country, year) %>%
+  pivot_longer( cols = c(EXPORT_LCU, IMPORT_LCU), names_to = "EXP_IMP", values_to = "AMT_LCU") %>% 
+  drop_na(AMT_LCU) %>%  
+  filter(year==2020) %>% 
+  ggplot(aes(x=EXP_IMP, y=log(AMT_LCU))) + geom_boxplot()  ## boxplot
+
+
 
 # 3. missing value 贸府 : drop
 df %>% filter(!region %in% c("","Aggregates")) %>%
   select(country:year, EXPORT_LCU) %>%
   arrange(country, year) %>%
-  pivot_wider( names_from = year, names_prefix = "IMP_", values_from = EXPORT_LCU ) %>% 
-  drop_na() %>% view()
+  pivot_wider( names_from = year, names_prefix = "EXP_", values_from = EXPORT_LCU ) %>% 
+  drop_na()
 
 
 # 3. missing value 贸府 : replace
 df %>% filter(!region %in% c("","Aggregates")) %>%
   select(country:year, EXPORT_LCU) %>%
   arrange(country, year) %>%
-  pivot_wider( names_from = year, names_prefix = "IMP_", values_from = EXPORT_LCU ) %>% 
-  replace_na(list(IMP_2000=0, IMP_2001=0)) %>% view()
-
-             
+  mutate(EXPORT_LCU = replace_na(EXPORT_LCU, 0)) %>%
+  # replace_na(list(EXPORT_LCU=0)) %>%
+  pivot_wider( names_from = year, names_prefix = "EXP_", values_from = EXPORT_LCU )
