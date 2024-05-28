4ab9b77a591c8a715431d3a8fefb2a263e3e1d02
datadigger01 <143661820+datadigger01@users.noreply.github.com>
2024-05-28 15:33:34 +0900

    Add files via upload

--- R_practice_panel_regression_1.R
+++ R_practice_panel_regression_1.R
@@ -8,7 +8,7 @@ df_lm <- df %>% drop_na(TO_export, TO_export_pre, GDP_growth_rate, GDP_gr_rate_p
                         Inflation_rate, Inflation_rate_pre, population, gdp_per_capita) %>% 
                 filter(!sub_region %in% c('Polynesia','Micronesia','Melanesia')) %>%
                 mutate(corona_yn = case_when(year >= 2020 & year <= 2021 ~ "Y",
-                                                                    TRUE ~ "N")) 
+                                                                    TRUE ~ "N"))
 # pooling OLS
 pooledOLS_result <- lm(data=df_lm, log(TO_export) ~ log(TO_export_pre) + 
                                                      GDP_growth_rate + 
@@ -24,19 +24,23 @@ summary(pooledOLS_result)
 plot(pooledOLS_result,1)
 
 # residual meaning
-# plot(log(df_lm$TO_export_pre),log(df_lm$TO_export))
-# lm_r <- lm(data=df_lm, log(TO_export) ~ log(TO_export_pre))
-# abline(a=lm_r$coefficients[1], b=lm_r$coefficients[2])
+plot(log(df_lm$TO_export_pre),log(df_lm$TO_export))
+lm_r <- lm(data=df_lm, log(TO_export) ~ log(TO_export_pre))
+abline(a=lm_r$coefficients[1], b=lm_r$coefficients[2])
 
-# names(pooledOLS_result)
-# check <- data.frame(cbind(df_lm$year, pooledOLS_result$fitted.values, pooledOLS_result$residuals))
-# names(check) <- c('year','Fitted_V','Residual')
-# check %>% ggplot() +
-#           geom_point(aes(x=Fitted_V, y=Residual, color=year)) +
-#           geom_hline(yintercept = 0, color='red', lwd=1) +
-#           theme_bw()
+names(pooledOLS_result)
+check <- data.frame(cbind(df_lm$year, pooledOLS_result$fitted.values, pooledOLS_result$residuals))
+names(check) <- c('year','Fitted_V','Residual')
+check %>% ggplot() +
+          geom_point(aes(x=Fitted_V, y=Residual, color=year)) +
+          geom_hline(yintercept = 0, color='red', lwd=1) +
+          theme_bw()
 
-
+ggplot(check) +
+  aes(x = Residual) +
+  geom_density( fill = "#112446") +
+  geom_vline(xintercept = 0, col='red') +
+  theme_minimal()
 
 # heterogeneity checking after applying panel linear regression.
 lm_test <- lm(data=df_lm, log(TO_export) ~ log(TO_export_pre) + 
@@ -46,9 +50,13 @@ lm_test <- lm(data=df_lm, log(TO_export) ~ log(TO_export_pre) +
                                            Inflation_rate_pre +
                                            log(population) +
                                            log(gdp_per_capita) +
-                                           iso3c
-                                           # factor(year)
+                                           factor(iso3c) +
+                                           factor(year)
               )
 summary(lm_test)
 names(lm_test)
 plot(lm_test,1)
+
+
+check1 <- data.frame(cbind(df_lm$year, lm_test$fitted.values, lm_test$residuals))
+names(check1) <- c('year','Fitted_V','Residual')
