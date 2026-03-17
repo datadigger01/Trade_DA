7e30d01d4d58cb295f5de62c8b94687872bf0498
datadigger01 <143661820+datadigger01@users.noreply.github.com>
2026-03-17 10:19:14 +0900

    Add files via upload

--- Script/2026S/world_bank_api.R
+++ Script/2026S/world_bank_api.R
@@ -1,52 +1,110 @@
-install.packages("WDI")
+#install.packages("WDI")
 library(WDI)
+library(tidyverse)
+
 
 # WDIsearch() 함수는 WDI 데이터베이스에서 특정 키워드와 일치하는 지표를 검색하는 데 사용됩니다.
 # string: 검색할 키워드입니다. 예를 들어, "export" 또는 "import"와 같은 단어를 입력할 수 있습니다.
 # field: 검색할 필드를 지정합니다. 일반적으로 "name"을 사용
 
-# import
+# export
 wdi_trade <- WDIsearch(string = "export", field = "name", short = FALSE ,cache = NULL)
 wdi_export <- WDI(country = "all", indicator = 'TX.VAL.MRCH.XD.WD', start = 2010, end = 2025)  #	TX.VAL.MRCH.XD.WD:Export value index (2015 = 100)
-# export
+#write_csv(wdi_export, "D:/wdi_export.csv")
+
+# import
 wdi_trade <- WDIsearch(string = "import", field = "name", short = FALSE ,cache = NULL)
 wdi_import <- WDI(country = "all", indicator = 'TM.VAL.MRCH.XD.WD', start = 2010, end = 2025)  #	TM.VAL.MRCH.XD.WD:Import value index (2015 = 100)
-# FDI
-wdi_fdi <- WDIsearch(string = "investment", field = "name", short = FALSE ,cache = NULL)
-wdi_fdi_out <- WDI(country = "all", indicator = 'BM.KLT.DINV.WD.GD.ZS', start = 2010, end = 2025)  # BM.KLT.DINV.WD.GD.ZS:Foreign direct investment, net outflows (% of GDP)
-# wdi_fdi_in <- WDI(country = "all", indicator = 'BN.KLT.DINV.CD.ZS', start = 2010, end = 2025)
+# GDP growth
+wdi_gdp <- WDIsearch(string = "gdp growth", field = "name", short = FALSE ,cache = NULL)
+wdi_gdpgrowth <- WDI(country = "all", indicator = 'NY.GDP.MKTP.KD.ZG', start = 2010, end = 2025)  # NV.AGR.TOTL.ZG:GDP growth (annual %)
+
+url <- "https://raw.githubusercontent.com/datadigger01/Trade_DA/main/Data/2026D/country_region.csv"
+country_info <- read_csv(url)
+
 
 # how to merge country_info with wdi_export dataset by iso2c code
-# Assuming you have a country_info dataset with a column named 'iso2c' and wdi_export dataset with a column named 'iso2c'
-# You can use the merge function in R to merge the two datasets based on the 'iso2c' column
-merged_data <- merge(country_info, wdi_export, by.x = "iso_2", by.y="iso2c", all.x = TRUE) %>% 
-  select(iso_2, country.x, year, TX.VAL.MRCH.XD.WD) %>% 
-  rename(country=country.x, export_value_index = TX.VAL.MRCH.XD.WD)
+# merged_data <- merge(country_info, wdi_export, by.x = "iso_2", by.y="iso2c", all.x = TRUE) %>% 
+#   select(iso_2, country.x, year, TX.VAL.MRCH.XD.WD) %>% 
+#   rename(country=country.x, export_value_index = TX.VAL.MRCH.XD.WD)
+str(country_info)
 
-merged_data <- merge(merged_data, wdi_import, by.x = c("iso_2","year"), by.y= c("iso2c","year"), all.x = TRUE) %>% 
-  select(iso_2, country.x, year, export_value_index, TM.VAL.MRCH.XD.WD) %>% 
-  rename(country=country.x, import_value_index = TM.VAL.MRCH.XD.WD)
 
-merged_data <- merge(merged_data, wdi_fdi_out, by.x = c("iso_2","year"), by.y= c("iso2c","year"), all.x = TRUE) %>% 
-  select(iso_2, country.x, year, export_value_index, import_value_index, BM.KLT.DINV.WD.GD.ZS) %>% 
-  rename(country=country.x, fdi_outflow_gdp = BM.KLT.DINV.WD.GD.ZS) 
+merged_data <- country_info %>%
+  left_join(wdi_export, by = c("iso_2" = "iso2c")) %>%
+  select(iso_2, iso_3, name, region, sub_region, year, export_value_index = TX.VAL.MRCH.XD.WD)
 
 str(merged_data)
+# merged_data <- merge(merged_data, wdi_import, by.x = c("iso_2","year"), by.y= c("iso2c","year"), all.x = TRUE) %>% 
+#   select(iso_2, country.x, year, export_value_index, TM.VAL.MRCH.XD.WD) %>% 
+#   rename(country=country.x, import_value_index = TM.VAL.MRCH.XD.WD)
+merged_data <- merged_data %>%
+  left_join(wdi_import, by = c("iso_2" = "iso2c", "year" = "year")) %>%
+  select(iso_2, iso_3, name, region, sub_region, year, export_value_index, import_value_index = TM.VAL.MRCH.XD.WD)
+
+merged_data <- merged_data %>%
+  left_join(wdi_gdpgrowth, by = c("iso_2" = "iso2c", "year" = "year")) %>%
+  select(iso_2, iso_3, name, region, sub_region, year, export_value_index, import_value_index, gdp_growth = NY.GDP.MKTP.KD.ZG)
 
 
+str(merged_data)
 
 
 # create line graph of export_value_index by country with merged_data
 library(ggplot2)
 
-# select several countries in the merged_data and draw the line graph with ggplot2
-countries_to_plot <- c('KR','JP','CN') # ISO2C codes for South Korea, Japan
-target_data <- merged_data %>% filter(iso_2 %in% countries_to_plot)
+#### select several countries in the merged_data and draw the line graph with ggplot2
+
+unique(merged_data$sub_region)
+target_countries <- c("Eastern Asia")
+target_data <- merged_data %>% filter(sub_region %in% target_countries) %>% 
+                                  filter(!name %in% c("Korea, Democratic People's Republic of","Macao","Mongolia"))
+unique(target_data$name)
+
+# unique(merged_data$sub_region)
+# target_countries <- c("South-eastern Asia")
+# target_data <- merged_data %>% filter(sub_region %in% target_countries) %>% 
+#                                 filter(!name %in% c('Brunei Darussalam','Timor-Leste'))
+# unique(target_data$name)
+
+unique(merged_data$sub_region)
+target_countries <- c("Western Europe")
+target_data <- merged_data %>% filter(sub_region %in% target_countries) %>%
+                              filter(!name %in% c('Liechtenstein'))
+unique(target_data$name)
 
 
-ggplot(target_data, aes(x = year, y = export_value_index, color =country)) +
-  geom_line() +
-  labs(title = "Export Value Index by Country", x = "Year", y = "Export Value Index") +
+###### line graph of export_value_index by country with target_data
+ggplot(target_data, aes(x = year, y = export_value_index, color = name)) +
+  geom_line(linewidth = 1.2) +
+  # scale_color_manual(values = c("Korea, Republic of" = "black", "China" = "red", "Japan" = "blue")) +
+  labs(title = "Export by Country", x = "Year", y = "Export Value Index") +
   theme_minimal() +
-  theme(legend.position = "bottom") +
-  guides(color = guide_legend(nrow = 2, byrow = TRUE)) # Adjust legend to have multiple rows
+  theme(legend.position = "bottom")
+
+ggplot(target_data, aes(x = year, y = gdp_growth, color = name)) +
+  geom_line(linewidth = 1.2) +
+  # scale_color_manual(values = c("Korea, Republic of" = "black", "China" = "red", "Japan" = "blue")) +
+  labs(title = "GDP growth by Country", x = "Year", y = "GDP growth") +
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
+
+##### Boxplot of export_value_index by sub_region with merged_data
+# write_csv(merged_data, "D:/merged_data.csv")
+unique(merged_data$sub_region)
+target_data <- merged_data %>%
+  filter(sub_region == "South-eastern Asia", year %in% c(2019, 2024)) %>%
+  mutate(year = as.factor(year))
+
+# GDP Growth Boxplot
+ggplot(target_data, aes(x = year, y = gdp_growth, fill = year)) +
+  geom_boxplot(linewidth = 0.7, outlier.shape = 21, outlier.size = 2) +
+  scale_fill_manual(values = c("2019" = "#457B9D", "2024" = "#E63946")) +
+  labs(
+    title = "GDP Growth(2019 vs 2024)",
+    x = "Year",
+    y = "GDP Growth (%)"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none")
