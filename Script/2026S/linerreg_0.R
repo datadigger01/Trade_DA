e9078430b0e400f2c6966fe34c27e3d285f6ec58
datadigger01 <143661820+datadigger01@users.noreply.github.com>
2026-03-31 07:30:42 +0900

    Add files via upload

---
+++ Script/2026S/linerreg_0.R
@@ -0,0 +1,102 @@
+# installation un_comtrade / worldbank open api
+
+# install.packages("comtradr")
+# install.packages("WDI")
+library("WDI")
+library("comtradr")
+
+library("tidyverse")
+library("dplyr")
+library("ggplot2")
+# library("ggrepel")
+
+
+url <- "https://raw.githubusercontent.com/datadigger01/Trade_DA/main/Data/2026D/country_region.csv"
+country_info <- read_csv(url)
+
+####################### World Bank Indicators ###########################
+### Export
+wdi_trade <- WDIsearch(string = "export", field = "name", short = FALSE ,cache = NULL)
+wdi_export1 <- WDI(country = "all", indicator = 'NE.EXP.GNFS.KD', start = 2010, end = 2025)  #	NE.EXP.GNFS.KD: Export of good and service(constant 2015 USD) 
+wdi_export2 <- WDI(country = "all", indicator = 'NE.EXP.GNFS.ZS', start = 2010, end = 2025)  #	NE.EXP.GNFS.ZD: Export of good and service(% of GDP) 
+#wdi_export3 <- WDI(country = "all", indicator = 'TX.VAL.MRCH.XD.WD', start = 2010, end = 2025) # Export volume index (2015 = 100)
+
+### Import
+wdi_trade <- WDIsearch(string = "import", field = "name", short = FALSE ,cache = NULL)
+wdi_import1 <- WDI(country = "all", indicator = 'NE.IMP.GNFS.KD', start = 2010, end = 2025)  #	NE.IMP.GNFS.ZS:Imports of goods and services(constant 2015 USD)
+wdi_import2 <- WDI(country = "all", indicator = 'NE.IMP.GNFS.ZS', start = 2010, end = 2025)  #	NE.EXP.GNFS.ZD: Export of good and service(% of GDP) 
+#wdi_import3 <- WDI(country = "all", indicator = 'TM.VAL.MRCH.XD.WD', start = 2010, end = 2025) 
+
+### GDP
+wdi_trade <- WDIsearch(string = "gdp", field = "name", short = FALSE ,cache = NULL)
+wdi_gdpgrowth <- WDI(country = "all", indicator = 'NY.GDP.MKTP.KD.ZG', start = 2010, end = 2025)  # NV.AGR.TOTL.ZG:GDP growth (annual %)
+wdi_gdp_per <- WDI(country = "all", indicator = 'NY.GDP.PCAP.KD', start = 2010, end = 2025)  # NY.GDP.PCAP.KD: GDP per Capita(constant 2015 USD)
+
+### Inflation
+wdi_trade <- WDIsearch(string = "consumer price", field = "name", short = FALSE ,cache = NULL)
+wdi_cpi <- WDI(country = "all", indicator = 'FP.CPI.TOTL', start = 2010, end = 2025)  # FP.CPI.TOTL: consumer price index(2010=100)
+
+### Population
+wdi_trade <- WDIsearch(string = "population", field = "name", short = FALSE ,cache = NULL)
+wdi_pop_t <- WDI(country = "all", indicator = 'SP.POP.TOTL', start = 2010, end = 2025) # SP.POP.TOTL : Population, total
+wdi_pop_1564 <- WDI(country = "all", indicator = 'SP.POP.1564.TO', start = 2010, end = 2025) # SP.POP.1564.TO : Population ages 15-64, total
+
+### FDI(Foreign Direct Investment) 
+wdi_trade <- WDIsearch(string = "investment", field = "name", short = FALSE ,cache = NULL)
+wdi_fdi_r <- WDI(country = "all", indicator = 'BX.KLT.DINV.WD.GD.ZS', start = 2010, end = 2025) # BX.KLT.DINV.WD.GD.ZS : Foreign direct investment, net inflows (% of GDP)
+# wdi_fdi_out <- WDI(country = "all", indicator = 'BM.KLT.DINV.CD.WD', start = 2010, end = 2025) # BX.KLT.DINV.WD.GD.ZS : Foreign direct investment, net inflows (% of GDP)
+# wdi_fdi_out <- WDI(country = "all", indicator = 'BN.KLT.DINV.CD.DRS', start = 2010, end = 2025) # BX.KLT.DINV.WD.GD.ZS : Foreign direct investment, net inflows (% of GDP)
+
+
+colnames(merged_data)
+merged_data <- country_info %>% 
+            inner_join(wdi_export1, by=c("iso_3"="iso3c")) %>% select(name, iso_2, iso_3, region, sub_region, year, export_value=NE.EXP.GNFS.KD) %>% 
+            left_join(wdi_export2, by=c("iso_3"="iso3c", "year"="year")) %>% 
+                  select(name, iso_2, iso_3, region, sub_region, year, export_value, export_r_gdp=NE.EXP.GNFS.ZS) %>%
+            left_join(wdi_import1, by=c("iso_3"="iso3c", "year"="year")) %>%
+                  select(name, iso_2, iso_3, region, sub_region, year, export_value, export_r_gdp, import_value=NE.IMP.GNFS.KD) %>% 
+            left_join(wdi_import2, by=c("iso_3"="iso3c", "year"="year")) %>%
+                  select(name, iso_2, iso_3, region, sub_region, year, export_value, export_r_gdp, import_value, import_r_gdp=NE.IMP.GNFS.ZS) %>%
+            left_join(wdi_gdpgrowth, by=c("iso_3"="iso3c", "year"="year")) %>% 
+                  select(name, iso_2, iso_3, region, sub_region, year, export_value, export_r_gdp, import_value, import_r_gdp, gdp_growth_r=NY.GDP.MKTP.KD.ZG) %>%
+            left_join(wdi_gdp_per, by=c("iso_3"="iso3c", "year"="year")) %>% 
+                  select(name, iso_2, iso_3, region, sub_region, year, export_value, export_r_gdp, import_value, import_r_gdp, gdp_growth_r, gdp_per_cap=NY.GDP.PCAP.KD) %>% 
+            left_join(wdi_cpi, by=c("iso_3"="iso3c", "year"="year")) %>%
+                  select(name, iso_2, iso_3, region, sub_region, year, export_value, export_r_gdp, import_value, import_r_gdp, gdp_growth_r, gdp_per_cap, cpi=FP.CPI.TOTL) %>% 
+            left_join(wdi_pop_t, by=c("iso_3"="iso3c", "year"="year")) %>%
+                  select(name, iso_2, iso_3, region, sub_region, year, export_value, export_r_gdp, import_value, import_r_gdp, gdp_growth_r, gdp_per_cap, cpi, pop_t=SP.POP.TOTL) %>% 
+            left_join(wdi_pop_1564, by=c("iso_3"="iso3c", "year"="year")) %>%
+                  select(name, iso_2, iso_3, region, sub_region, year, export_value, export_r_gdp, import_value, import_r_gdp, gdp_growth_r, gdp_per_cap, cpi, pop_t, pop_1564=SP.POP.1564.TO) %>% 
+            left_join(wdi_fdi_r, by=c("iso_3"="iso3c", "year"="year")) %>%
+                  select(name, iso_2, iso_3, region, sub_region, year, export_value, export_r_gdp, import_value, import_r_gdp, gdp_growth_r, gdp_per_cap, cpi, pop_t, pop_1564, fdi_r_gdp=BX.KLT.DINV.WD.GD.ZS)
+
+# create new variable: population ratio of 15-64 to total population
+merged_data <- merged_data %>%
+                mutate(pop1564_r = round(pop_1564/pop_t, 4) * 100)
+
+  unique(merged_data$sub_region)
+merged_data %>% filter(year >= 2020) %>% 
+                # filter(fdi_r_gdp >= -10 & fdi_r_gdp <= 10 ) %>%
+                drop_na(gdp_growth_r, export_r_gdp) %>% 
+          ggplot(mapping = aes(y=log(export_value),x=log(gdp_per_cap))) +
+          geom_point() +
+          geom_smooth(method = "lm", se=F) +
+          theme_classic()
+
+
+## OLS regression
+reg_data <- merged_data %>% filter(year >= 2010) %>% drop_na(export_value, gdp_per_cap, cpi)
+reg_model <- lm(log(export_value) ~  log(lag(gdp_per_cap)) 
+                                      # + log(lag(export_value,2))
+                                      # + cpi
+                                      # + fdi_r_gdp
+                                      # + log(pop_t)
+                                      , 
+                                      data = reg_data)
+summary(reg_model)
+# plot(reg_model,1)
+
+plot(reg_model$fitted.values, reg_model$residuals, pch=16, main="Residuals vs Fitted Values", xlab="Fitted Values", ylab="Residuals", ylim=c(-5,5))
+abline(h=0, col="red", lwd=2)
+
+
---
+++ Script/2026S/world_bank_ttest_1.R
@@ -0,0 +1,136 @@
+#install.packages("WDI")
+library(WDI)
+library(tidyverse)
+library(ggplot2)
+
+# WDIsearch() 함수는 WDI 데이터베이스에서 특정 키워드와 일치하는 지표를 검색하는 데 사용됩니다.
+# string: 검색할 키워드입니다. 예를 들어, "export" 또는 "import"와 같은 단어를 입력할 수 있습니다.
+# field: 검색할 필드를 지정합니다. 일반적으로 "name"을 사용
+
+# export
+wdi_trade <- WDIsearch(string = "export", field = "name", short = FALSE ,cache = NULL)
+wdi_export1 <- WDI(country = "all", indicator = 'NE.EXP.GNFS.ZS', start = 2010, end = 2025)  # NE.EXP.GNFS.ZS: Exports of goods and services (% of GDP)
+wdi_export2 <- WDI(country = "all", indicator = 'TX.VAL.MRCH.XD.WD', start = 2010, end = 2025) # Export value index (2015 = 100)	
+
+
+# import
+wdi_trade <- WDIsearch(string = "import", field = "name", short = FALSE ,cache = NULL)
+wdi_import1 <- WDI(country = "all", indicator = 'NE.IMP.GNFS.ZS', start = 2010, end = 2025)  # NE.IMP.GNFS.ZS:Imports of goods and services (% of GDP)
+wdi_import2 <- WDI(country = "all", indicator = 'TM.VAL.MRCH.XD.WD', start = 2010, end = 2025)  # Import value index (2015 = 100)	
+
+
+# GDP growth
+wdi_gdp <- WDIsearch(string = "gdp growth", field = "name", short = FALSE ,cache = NULL)
+wdi_gdpgrowth <- WDI(country = "all", indicator = 'NY.GDP.MKTP.KD.ZG', start = 2010, end = 2025)  # NV.AGR.TOTL.ZG:GDP growth (annual %)
+
+url <- "https://raw.githubusercontent.com/datadigger01/Trade_DA/main/Data/2026D/country_region.csv"
+country_info <- read_csv(url)
+
+
+# how to merge country_info with wdi_export dataset by iso2c code
+# merged_data <- merge(country_info, wdi_export, by.x = "iso_2", by.y="iso2c", all.x = TRUE) %>% 
+#   select(iso_2, country.x, year, TX.VAL.MRCH.XD.WD) %>% 
+#   rename(country=country.x, export_value_index = TX.VAL.MRCH.XD.WD)
+str(country_info)
+
+
+merged_data <- country_info %>%
+  left_join(wdi_export1, by = c("iso_2" = "iso2c")) %>%
+  select(iso_2, iso_3, name, region, sub_region, year, export_ratio = NE.EXP.GNFS.ZS)
+
+str(merged_data)
+
+# merged_data <- merge(merged_data, wdi_import, by.x = c("iso_2","year"), by.y= c("iso2c","year"), all.x = TRUE) %>% 
+#   select(iso_2, country.x, year, export_value_index, TM.VAL.MRCH.XD.WD) %>% 
+#   rename(country=country.x, import_value_index = TM.VAL.MRCH.XD.WD)
+merged_data <- merged_data %>%
+  left_join(wdi_import1, by = c("iso_2" = "iso2c", "year" = "year")) %>%
+  select(iso_2, iso_3, name, region, sub_region, year, export_ratio, import_ratio = NE.IMP.GNFS.ZS)
+
+merged_data <- merged_data %>%
+  left_join(wdi_gdpgrowth, by = c("iso_2" = "iso2c", "year" = "year")) %>%
+  select(iso_2, iso_3, name, region, sub_region, year, export_ratio, import_ratio, gdp_growth = NY.GDP.MKTP.KD.ZG)
+
+
+unique(merged_data$region)
+# calculate mean of gdp_growth rate by region
+merged_data %>% filter(year==2020, region %in% c('Asia','Americas')) %>% 
+                group_by(region) %>%  
+                summarise(avg_gdpgrowth = mean(gdp_growth, na.rm = TRUE), .groups = "drop")
+
+# distribution by region or sub_region  
+merged_data %>% filter(year==2020, region %in% c('Asia','Americas')) %>% 
+            # filter(!iso_3 %in% c('PSE','TLS','GUY','VEN')) %>%
+            select(iso_3, name, region, sub_region,year, gdp_growth) %>% drop_na(gdp_growth) %>% 
+            ggplot(data=., aes(gdp_growth, fill = region)) +
+            geom_density(alpha=0.5) +
+            # geom_vline(xintercept =-4.55, color='red') +
+            # geom_vline(xintercept =-9.55, color='blue') +
+            theme_bw()
+
+
+###################################################################################
+### Independent sample t-test for GDP growth rate between Asia and Americas in 2020
+###################################################################################
+t_test_data <- merged_data %>%
+  filter(region %in% c("Asia","Americas"), year==2020) %>%
+  select(name, iso_3, region, year, gdp_growth) %>% drop_na(gdp_growth)
+
+# perform independent sample t-test
+t.test(gdp_growth ~ region, data = t_test_data)
+
+# t_test_result <- t.test(gdp_growth ~ region, data = t_test_data)
+# t_test_result$statistic
+
+# Another way of performing independent sample t-test using pipe operator
+###############################################################################
+merged_data %>%
+  filter(region %in% c("Asia","Americas"), year==2020) %>%
+  select(name, iso_3, region, year, gdp_growth) %>% drop_na(gdp_growth) %>%
+  with(t.test(gdp_growth ~ region, data = .))
+  
+
+
+
+
+##############################################################################################
+## Paired sample t-test for GDP growth rate between Asia and Americas before COVID/after COVID
+##############################################################################################
+unique(merged_data$region)
+merged_data %>% filter(year %in% c(2019,2023), region %in% c('Asia')) %>% 
+  # filter(!iso_3 %in% c('PSE','TLS')) %>%
+  select(name, iso_3, region, sub_region, year, gdp_growth) %>% drop_na(gdp_growth) %>% 
+  ggplot(data=., aes(gdp_growth, fill = as.factor(year))) +
+  geom_density(alpha=0.7) +
+  # geom_vline(xintercept =3.84, color='red') +
+  # geom_vline(xintercept =3.03, color='blue') +
+  theme_bw()
+
+# # calculate mean of gdp_growth rate
+merged_data %>% filter(year %in% c(2019, 2023), region %in% c("Americas"))  %>%
+  # filter(!iso_3 %in% c('GUY','VEN')) %>%
+  select(name, iso_3, region, sub_region, year, gdp_growth) %>% drop_na(gdp_growth) %>%
+  group_by(year) %>%
+  summarise(avg_gdpgrowth = mean(gdp_growth, na.rm = TRUE), .groups = "drop")
+
+# paired t-test for GDP growth between 2019 and 2024
+paired_t_test_data <- merged_data %>%
+  filter(region %in% c("Americas"), year %in% c(2019, 2023)) %>% 
+  # filter(!iso_3 %in% c('PSE','VEN')) %>%
+  select(name, iso_3, region, sub_region, year, gdp_growth) %>% 
+  pivot_wider(id_cols = c(name,iso_3,region), names_from = year, values_from = gdp_growth, names_prefix = "gdp_growth_") %>% 
+  drop_na(gdp_growth_2019, gdp_growth_2023)
+# perform paired t-test
+t.test(paired_t_test_data$gdp_growth_2023, paired_t_test_data$gdp_growth_2019, paired = TRUE)
+
+
+# Another way of performing paired t-test using pipe operator
+#################################################################
+unique(merged_data$region)
+merged_data %>% 
+  filter(region == "Americas", year %in% c(2019, 2023)) %>%
+  # filter(!iso_3 %in% c('PSE','VEN')) %>%
+  select(name, iso_3, region, sub_region, year, gdp_growth) %>% drop_na(gdp_growth) %>%
+  pivot_wider(id_cols = c(name,iso_3,region), names_from = year, values_from = gdp_growth, names_prefix = "gdp_growth_") %>%
+  drop_na(gdp_growth_2019, gdp_growth_2023) %>%
+  with(t.test(gdp_growth_2023,gdp_growth_2019, paired = TRUE, data = .))
